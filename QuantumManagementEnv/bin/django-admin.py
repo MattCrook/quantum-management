912760850eaf7bf5f501b05c9b14daae4327c084
Matt Crook <matt.crook11@gmail.com>
2020-06-12 09:49:28 -0500

    initial commit

---
+++ QuantumManagementEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/matthewcrook/code/nss/backEnd/capstone/quantummanagement/QuantumManagementEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

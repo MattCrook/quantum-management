a14e18ed0bb15878d5a1b8bcd1aafabd33aa41d0
Matt Crook <matt.crook11@gmail.com>
2020-06-18 18:22:12 -0500

    feat: employee list and styles.

--- quantummanagementapp/views/employees/employee_list.py
+++ quantummanagementapp/views/employees/employee_list.py
@@ -14,8 +14,54 @@ from django.contrib.auth.models import User
 def employee_list(request):
     if request.method == 'GET':
         employees = Employee.objects.all()
+        print("EMPLOYEES", employees)
+
+        role_groups = {}
+        roles = list()
+
+        for employee in employees:
+            role = employee.role
+            if role not in roles:
+                roles.append(role)
+            else:
+                pass
+        print("roles", roles)
+
+
+
+
+
+
+        # for employee in employees:
+        #     if employee.id not in role_groups:
+        #         role_groups[employee.id] = role
+        #         role_groups[employee.id].role.append(employee)
+        #     else:
+        #         role_groups[employee.id].role.append(employee)
+        # print("ROLEGOUPS", role_groups)
+
+
+
+
+        # Iterate the list of tuples
+        # for (library, book) in libraries:
+
+    # If the dictionary does have a key of the current
+    # library's `id` value, add the key and set the value
+    # to the current library
+    # if library.id not in library_groups:
+    #     library_groups[library.id] = library
+    #     library_groups[library.id].books.append(book)
+
+    # If the key does exist, just append the current
+    # book to the list of books for the current library
+    # else:
+    #     library_groups[library.id].books.append(book)
+
+
         template = 'employees/employee_list.html'
         context = {
-            'employees': employees
+            'employees': employees,
+            'roles': roles
         }
         return render(request, template, context)

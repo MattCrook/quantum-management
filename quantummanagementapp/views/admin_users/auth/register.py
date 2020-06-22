31041d41dab1aa932d15b9f28d61b4dbb7a4e035
Matt Crook <matt.crook11@gmail.com>
2020-06-22 10:10:45 -0500

    feat: registration form.

--- quantummanagementapp/views/admin_users/auth/register.py
+++ quantummanagementapp/views/admin_users/auth/register.py
@@ -1,10 +1,26 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect, reverse
+from quantummanagementapp.models import SignUpForm
+
+# def register_user(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect(reverse('quantummanagementapp:home'))
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'registration/register.html', {'form': form})
+
 
 def register_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -13,5 +29,5 @@ def register_user(request):
             login(request, user)
             return redirect(reverse('quantummanagementapp:home'))
     else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+        form = SignUpForm()
+        return render(request, 'registration/register.html', {'form': form})

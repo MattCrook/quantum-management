55d8c7375d90a390c86bc2ca745a2fe40286e9a6
Matt Crook <matt.crook11@gmail.com>
2020-06-23 14:37:14 -0500

    fix: add park form and styles.

--- quantummanagementapp/static/scripts/buttonHandlers.js
+++ quantummanagementapp/static/scripts/buttonHandlers.js
@@ -1,4 +1,13 @@
 const deleteEmployeeButton = document.querySelector(".employee_detail_delete_btn");
-deleteEmployeeButton.addEventListener("click", () => {
-    window.alert("Are you sure you want to delete this employee?");
-});
+if (deleteEmployeeButton) {
+    deleteEmployeeButton.addEventListener("click", () => {
+        window.alert("Are you sure you want to delete this employee?");
+    });
+};
+
+const createNewParkButton = document.getElementById("create_park_btn");
+if (createNewParkButton) {
+    createNewParkButton.addEventListener("click", () => {
+        window.alert("Click OK to confirm creation of New Theme Park.")
+    });
+};

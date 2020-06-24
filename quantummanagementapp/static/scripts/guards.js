cf9ab73502ea764057e93d44ac854abed786dfa6
Matt Crook <matt.crook11@gmail.com>
2020-06-24 10:58:03 -0500

    fix: guard for pay rate added.

--- quantummanagementapp/static/scripts/guards.js
+++ quantummanagementapp/static/scripts/guards.js
@@ -2,21 +2,15 @@ const isSalary = document.getElementById("is_salary");
 const isHourly = document.getElementById("is_hourly");
 const startDate = document.getElementById("start_date");
 const updateEmployeeButton = document.getElementById("update_employee_btn");
+const formContainer = document.getElementById("form_container")
 
-if (updateEmployeeButton && isHourly && isSalary) {
-    console.log("called")
-  updateEmployeeButton.addEventListener("click", () => {
-    if (isSalary === "" || isHourly === "") {
-      window.alert("Please select a pay rate.");
-    }
-  });
-}
-
-if (updateEmployeeButton) {
-    updateEmployeeButton.addEventListener("click", () => {
-      console.log("updatebtnclick")
-    if (startDate === "") {
-      window.alert("Please select a start date.");
-    }
-  });
-}
+// if (updateEmployeeButton && isHourly && isSalary) {
+//     updateEmployeeButton.addEventListener("click", () => {
+//         const salary = isSalary.value;
+//         const hourly = isHourly.value;
+//         console.log(salary)
+//     if ((salary === undefined) && (hourly === undefined)) {
+//       window.alert("Please select a pay rate.");
+//     }
+//   });
+// }

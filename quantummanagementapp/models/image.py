34348e81d156dcd1ccbb63e451215ab70160146c
Matt Crook <matt.crook11@gmail.com>
2020-06-25 15:44:23 -0500

    fix: image feild only has one row image.

--- quantummanagementapp/models/image.py
+++ quantummanagementapp/models/image.py
@@ -3,7 +3,6 @@ from quantummanagementapp.models import AdminUser
 
 class Image(models.Model):
     image = models.ImageField(upload_to='images')
-    admin_user = models.ForeignKey(AdminUser, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.admin_user.user.first_name
+        return self.image

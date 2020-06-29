fdc834d2339be0074117f082190c6496068e8a41
Matt Crook <matt.crook11@gmail.com>
2020-06-29 10:16:10 -0500

    fix: gitignore

--- QuantumManagementEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/matthewcrook/code/nss/backEnd/capstone/quantummanagement/QuantumManagementEnv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

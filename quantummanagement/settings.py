8b17925124d5b01fe45c4407e3af900296a3bcb7
Matt Crook <matt.crook11@gmail.com>
2020-10-22 15:55:21 -0500

    collect static files.

--- quantummanagement/settings.py
+++ quantummanagement/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 LOGIN_REDIRECT_URL = 'home/'
 LOGIN_URL = 'login/'
---
+++ static/scripts/services.js
@@ -0,0 +1,114 @@
+export async function getParkList() {
+  try {
+    const response = await fetch(`/api/parks`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error("Request Failed");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getEmployeeList() {
+  try {
+    const response = await fetch(`/api/employees`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error("Request Failed");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function retrieveAttraction(id) {
+  try {
+    const response = await fetch(`/api/attractions/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error("Request Failed");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getParkAttractions(parkId) {
+  try {
+    const response = await fetch(`/api/park_attractions?park_id=${parkId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error("Request Failed");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getAttractionType(typeId) {
+  try {
+    const response = await fetch(`/api/attraction_types/${typeId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error("Request Failed");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+export async function getVisitorsList(parkId) {
+  try {
+    const response = await fetch(`/api/visitors?park_id=${parkId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error("Request Failed");
+  } catch (err) {
+    console.log(err);
+  }
+}

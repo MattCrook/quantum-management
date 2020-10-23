0286f689386b2eb12652dd1cef577f48d8934fad
Matt Crook <matt.crook11@gmail.com>
2020-10-22 19:32:06 -0500

    docker wip #3.

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # start-server
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd quantummanagement; python manage.py createsuperuser --no-input)
+    (cd app; python manage.py createsuperuser --no-input)
 fi
-(cd quantummanagement; gunicorn quantummanagement.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd app; gunicorn quantummanagement.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
